0e0f1c4b110f30552bd5a148efa99b023c073d7f
ahmed5768 <101082079+ahmed5768@users.noreply.github.com>
2024-05-18 02:14:26 +0500

    done

---
+++ app.js
@@ -0,0 +1,80 @@
+// Task no 01
+// var date = new Date()
+// console.log(date);
+
+//Task no 02
+// function user(name1, name2){
+//     var name1 = prompt("Enter your firstname")
+//     var name2 = prompt("Enter your lastname")
+//     var fullName = name1 + " " + name2
+//     console.log(fullName);
+// }
+// user()
+
+//Task no 03
+// function userInput(value1, value2){
+//     var value1 = +prompt("Enter a number")
+//     var value2 = +prompt("Enter a second number")
+//     var finalValue = value1 + value2
+//     console.log(finalValue);
+// }
+// userInput()
+
+
+//Task no 04
+// function computeOperation(num1, num2, operator) {
+//     var num1 = +prompt("Enter a value")
+//     var operator = prompt("Enter a operator")
+//     var num2 = +prompt("Enter a second value")
+//     let result;
+  
+//     switch (operator) {
+//       case '+':
+//         result = num1 + num2;
+//         break;
+//       case '-':
+//         result = num1 - num2;
+//         break;
+//       case '*':
+//         result = num1 * num2;
+//         break;
+//       case '/':
+//         result = num1 / num2;
+//         break;
+//       default:
+//         return "Invalid operator!";
+//     }
+
+//     console.log(result);
+//   }
+  
+//   computeOperation()
+
+
+//Task no 05
+// function square(num1){
+//     var num1 = +prompt("Enter a number")
+//     var finalValue = num1 * num1
+//     console.log(finalValue);
+// }
+// square()
+
+//Task no 06
+// function factorial(factSum){
+//     var userValue = +prompt("Enter a number for finding factorial")
+//     for(i = 1 ; i <= userValue ; i++){
+//         factSum = factSum * i
+//     }
+//     console.log("The factorial of " + userValue + " is " + factSum);
+// }
+// factorial(1)
+
+//Task no 07
+// function counting(start, end){
+//     var start = +prompt("Enter a number start of counting")
+//     var end = +prompt("Enter a number end of counting")
+//     for(i = start ; i <= end ; i++){
+//         console.log(i);
+//     }
+// }
+// counting()
